ef27f812fbcb5f92049020af3fec5cd76bc2e1dd
nicholashaynes1 <nicholashaynes1@gmail.com>
2015-12-11 12:35:48 -0700

    Initial Commit

---
+++ StupidApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StupidApp2
+//
+//  Created by Haynes, Nicholas on 12/11/15.
+//  Copyright © 2015 Haynes, Nicholas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
